0d8f41bb80befb6dabbb56b0ca2d7c37bcf05b25
Martin Hawksey <m.hawksey@gmail.com>
2014-07-03 16:28:50 +0100

    Initial

---
+++ conferencer-bp-addon.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   Plugin_Name
+ * @author    mhawksey <martin.hawksey@alt.ac.uk>
+ * @license   GPL-2.0+
+ * @link      http://alt.ac.uk
+ * @copyright 2014 Association for Learning Technology
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Conferencer BuddyPress Add-on
+ * Plugin URI:        https://github.com/mhawksey/conferencer-bp-addon
+ * Description:       Conferencer Add-on to integrate with BuddyPress
+ * Version:           1.0.0
+ * Author:            mhawksey
+ * Author URI:        http://mashe.hawksey.info
+ * Text Domain:       conferencer-bp-addon-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/mhawksey/conferencer-bp-addon
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
+
